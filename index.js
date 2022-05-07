066704d8fe6f32590b07ba868541bdf24229125b
Calypso Kay Riebe <calypso@Calypsos-MacBook-Pro.local>
2022-05-07 13:10:58 -0400

    finished with prettier so thomad can grade easier

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let greenCharacter = document.createElement('img')
+/* let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
@@ -8,4 +8,37 @@ document.body.append(greenCharacter)
 let pineTree = document.createElement('img')
 pineTree.src = 'assets/pine-tree.png'
 pineTree.style.position = 'fixed'
-pineTree.style.position = 
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
+*/
+
+function newImage(url, left, bottom) {
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left
+    object.style.bottom = bottom
+    document.body.append(object)
+    return object
+}
+
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pine-tree.png', '450px', '200px')
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+
+
+function newItem(object){
+   
+    object.addEventListener('dblclick', function(){
+        object.remove()
+    })
+    return object
+}
+
+newItem(newImage('assets/sword.png', '500px', '405px'))
+newItem(newImage('assets/sheild.png', '165px', '185px'))
+newItem(newImage('assets/staff.png','600px', '100px'))
