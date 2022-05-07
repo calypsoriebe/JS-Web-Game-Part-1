193b05cf1c22ce5bb73e523a1e29cef4e508df4f
Calypso Kay Riebe <calypso@Calypsos-MacBook-Pro.local>
2022-05-07 13:09:04 -0400

    finished so far

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.position = 
